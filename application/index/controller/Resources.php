d8e8157faa3385cefadc9b32b906a5116622bc17
seek-concer <1105420247@qq.com>
2018-06-22 14:13:46 +0800

    修改

---
+++ application/index/controller/Resources.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 资源下载
+ */
+
+namespace app\index\controller;
+use think\Controller;
+
+class Resources extends Controller
+{
+    /*======= 资源下载 ======*/
+    public function index()
+    {
+        return view();
+    }
+}
