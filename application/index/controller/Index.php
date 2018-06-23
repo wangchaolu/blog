299129b120547c3f18fd3bda05be0d0ab9e0bad9
seek-concer <1105420247@qq.com>
2018-06-23 14:25:06 +0800

    logo替换  数据库连接

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -10,7 +10,13 @@ class Index extends Controller
     /*======= 首页 ======*/
     public function index()
     {
-        
+        /*------ 首页banner图 ------*/
+//        $model = model('Ad');
+//        $banner_info = $model
+//            ->field(['img_url','jump_url'])
+//            ->where(['type'=>1,'status'=>1])
+//            ->select();
+//        dump($banner_info);
         $this->assign('action',request()->controller());
         return view();
     }
